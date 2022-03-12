580f7eee2f18a23ce4d2ac5bb6cbefc2e7b95c3d
Jeff Vergne <vergnejf@gmail.com>
2022-03-12 15:33:35 +0100

    completing part 1 of ravenous

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import './SearchBar.css';
+
+const searchByOptions = {
+    "Best Match": "best_match",
+    "Highest Rate": "rating",
+    "Most Reviewed": "review_count"
+};
+
+class SearchBar extends React.Component {
+    renderSortByOptions() {
+        const keys = Object.keys(searchByOptions); //fournit les keys correspond à l'objet searchByOptions (string)
+        return keys.map(sortByOption => {
+            let sortByOptionValue = searchByOptions[sortByOption];
+            return <li key={sortByOptionValue}>{sortByOption}</li>
+        }); 
+    }
+
+    render(){
+        return (
+        <div className="SearchBar">
+            <div className="SearchBar-sort-options">
+                <ul>
+                    {this.renderSortByOptions()}
+                </ul>
+            </div>
+            <div className="SearchBar-fields">
+                <input placeholder="Search Businesses" />
+                <input placeholder="Where?" />
+            </div>
+            <div className="SearchBar-submit">
+                <a>Let's Go</a>
+            </div>
+        </div>
+        );
+    }
+}
+
+export default SearchBar;
